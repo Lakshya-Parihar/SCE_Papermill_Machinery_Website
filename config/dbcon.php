425f4b44c03a4b193c9d94075303b467f1ea5657
lakshya <105096466+Lakshya-Parihar@users.noreply.github.com>
2024-05-25 10:05:55 +0530

    Add files via upload

---
+++ config/dbcon.php
@@ -0,0 +1,14 @@
+<?php
+
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $database = "sce_new";
+
+    $conn = mysqli_connect($servername,$username,$password,$database);
+    if(!$conn)
+    {
+        echo'connection failed';
+    }  
+    
+?>
