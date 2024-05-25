1627c3beb49a35a78177f24ec04771fd2ac39ac8
lakshya <105096466+Lakshya-Parihar@users.noreply.github.com>
2024-05-25 10:35:02 +0530

    Add files via upload

---
+++ includes/navbar.php
@@ -0,0 +1,32 @@
+<html>
+<link type="text/css" href="main.css">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+
+    <header>
+
+        <input type="checkbox" name="toggler" id="toggler">
+        <label for="toggler" class="fas fa-bars"></label>
+
+        <a href="#" class="logo">SCE<span>!!</span></a>
+
+        <nav class="navbar">
+            <a href="#home">HOME</a>
+            <a href="#about">ABOUT</a>
+            <a href="#products">PRODUCTS</a>
+            <a href="#faq">FAQ</a>
+            <a href="login.php">CONTACT</a>
+        </nav>
+
+        <div class="icons">
+            <a href="login.php" class="fas fa-check"></a>
+            <a href="login.php" class="fas fa-shopping-cart"></a>
+            <a href="login.php" class="fas fa-user"></a>
+        </div>
+        
+        <div class="btns">
+        <a href="./login.php" class="btncontent">Login</a>&nbsp;&nbsp;
+        <a href="./register.php" class="btncontent">Register</a>
+        </div>
+
+    </header>
+</html>
